b6cba8304d4f515dcdeb6952db770ceeaba92052
chrisbrickey <cbrickey1@gmail.com>
2017-07-18 13:36:08 -0700

    add comment for Jbuilder camelize

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# Allows backend column names to be in snake_case while camelCase used on front end
 Jbuilder.key_format camelize: :lower
