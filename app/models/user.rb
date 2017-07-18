20fd1e94556b1f143c0c49f4abf733e8bf16d5e3
chrisbrickey <cbrickey1@gmail.com>
2017-07-18 14:28:29 -0700

    add methods, correct typo in password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   validates :password,
               length: { minimum: 6 },
-              allow_nil: :true
+              allow_nil: true
 
   validates :name,
               presence: true,
@@ -24,5 +24,38 @@ class User < ApplicationRecord
   has_many :comments
   has_many :followers  #other users that are following this user
   has_many :followings #other users that this user is following
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+
+    if user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
 
 end
