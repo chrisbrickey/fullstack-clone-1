1a3587ed78402aa4d387be6508e80db3fb822abe
chrisbrickey <cbrickey1@gmail.com>
2017-07-18 16:50:17 -0700

    draft initial routes, including nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "static_pages#root"
+
+
+  namespace :api, defaults: { format: :json } do
+
+    resources :comments, only: %i[destroy], shallow: true do
+      resources :likes, only: %i[create destroy]
+    end
+
+    resources :follows, only: %i[destroy]
+
+    resources :photos, only: %i[index show create update destroy], shallow: true do
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create destroy]
+    end
+
+    resource :session, only: %i[create destroy]
+
+    resources :users, only: %i[index show create update], shallow: true do
+      resources :follows, only: %i[create]
+      resources :photos, only: %i[index]
+    end
+  end
+
+
 end
