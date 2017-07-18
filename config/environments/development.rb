259d9c7b67be718c9e8d7c05ef76fe0e6c133b98
chrisbrickey <cbrickey1@gmail.com>
2017-07-18 10:04:31 -0700

    add LiveReload capability

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #Reload browser every time a file is saved.
+  config.middleware.insert_before Rack::Runtime, Rack::LiveReload
+
   # Do not eager load code on boot.
   config.eager_load = false
 
