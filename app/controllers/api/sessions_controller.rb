418de0e40da7ad6c8253017ec0d274cf419d96df
chrisbrickey <cbrickey1@gmail.com>
2017-07-18 20:31:05 -0700

    draft initial methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,41 @@
 class Api::SessionsController < ApplicationController
+
+  #login
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      render "/"
+    else
+      render(
+        json: ["invalid username/password combination"],
+        status: 401
+      )
+    end
+
+  end
+
+  #logout
+  def destroy
+    @user = current_user
+
+    #Should I be rendering an empty object instead? {}
+    if @user
+      logout
+      #how will it know this is a POST request NOT a DELETE request?
+      render "/api/session"
+
+    else
+      render(
+        json: ["please sign up or log in"],
+        status: 404
+      )
+
+    end
+  end
+
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,52 @@
 class Api::UsersController < ApplicationController
+
+  #load all users
+  def index
+    @users = User.all
+  end
+
+  #show one user
+  def show
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+  end
+
+  #create a user (new account)
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render "/"
+    else
+      render(
+        json: @user.errors.full_messages,
+        status: 422
+      )
+    end
+  end
+
+  #edit a user
+  def update
+    @user = User.find(params[:id])
+
+    #why do we want the object to be returned even if it doesn't pass validation (functionality of update vs update_attributes)
+    if @user.update(user_params)
+      render "/"
+    else
+      render(
+        json: @user.errors.full_messages,
+        status: 422
+      )
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
