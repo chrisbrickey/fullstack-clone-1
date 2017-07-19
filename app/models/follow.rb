635818fdde59c20bc6e1375dad80a18a28633ff9
chrisbrickey <cbrickey1@gmail.com>
2017-07-19 10:56:17 -0700

    correct following_id typo (make symbol)

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,7 +2,7 @@ class Follow < ApplicationRecord
   validates :follower_id, :following_id, presence: true
 
   validates :follower_id,
-              uniqueness: { scope: following_id,
+              uniqueness: { scope: :following_id,
               message: "you are already following this user"}
 
   belongs_to :follower  #the user that is following another user
