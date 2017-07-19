3fd8c4fb97eab9f55c63dd549ef60ae3a0bf200f
chrisbrickey <cbrickey1@gmail.com>
2017-07-19 14:38:49 -0700

    add new methods to the window to test

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,13 +2,31 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ajaxSignup, ajaxLogin, ajaxLogout } from './util/session_api_util';
+
+// import { ajaxSignup, ajaxLogin, ajaxLogout } from './util/session_api_util';
+import { receiveCurrentUser,
+         receiveErrors,
+         signupCreateUser,
+         loginCreateSession,
+         logoutDestroySession } from './actions/session_actions';
+import { configureStore } from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  window.ajaxSignup = ajaxSignup;
-  window.ajaxLogin = ajaxLogin;
-  window.ajaxLogout = ajaxLogout;
+  // window.ajaxSignup = ajaxSignup;
+  // window.ajaxLogin = ajaxLogin;
+  // window.ajaxLogout = ajaxLogout;
+
+  window.receiveCurrentUser = receiveCurrentUser;
+  window.receiveErrors = receiveErrors;
+  window.signupCreateUser = signupCreateUser;
+  window.loginCreateSession = loginCreateSession;
+  window.logoutDestroySession = logoutDestroySession;
+
+
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
   ReactDOM.render(<h3>Instagram...coming from entry.jsx</h3>, root);
