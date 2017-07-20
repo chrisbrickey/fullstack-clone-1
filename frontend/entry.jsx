1cc212cd4fcaa6b5d2a0453ee6dfcfe773d2e71b
chrisbrickey <cbrickey1@gmail.com>
2017-07-20 10:40:16 -0700

    correct capitalization of Root on import

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Root } from './components/root';
+import { Root } from './components/Root';
 import { configureStore } from './store/store';
 
 import { ajaxSignup, ajaxLogin, ajaxLogout } from './util/session_api_util';
