dee5e460572a823c86833db9fbb311664373fdb7
chrisbrickey <cbrickey1@gmail.com>
2017-07-21 19:11:08 -0700

    capitalize class name

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,15 @@ User.destroy_all
 
 #will rollback all seeds if any issues; create! fails loudly so can get more information on errors
 ActiveRecord::Base.transaction do
-  demo1 = user.create!(name: 'Demo User1', username: 'demouser1', password: "123456")
-  demo2 = user.create!(name: 'Demo User2', username: "demouser2", password: "123456")
-  demo3 = user.create!(name: 'Demo User3', username: "demouser3", password: "123456")
-  demo4 = user.create!(name: 'Demo User4', username: "demouser4", password: "123456")
-  demo5 = user.create!(name: 'Demo User5', username: "demouser5", password: "123456")
-  demo6 = user.create!(name: 'Demo User6', username: "demouser6", password: "123456")
-  demo7 = user.create!(name: 'Demo User7', username: "demouser7", password: "123456")
-  demo8 = user.create!(name: 'Demo User8', username: "demouser8", password: "123456")
-  demo9 = user.create!(name: 'Demo User9', username: "demouser9", password: "123456")
-  demo10 = user.create!(name: 'Demo User10', username: "demouser10", password: "123456")
+  demo1 = User.create!(name: 'Demo User1', username: 'demouser1', password: "123456")
+  demo2 = User.create!(name: 'Demo User2', username: "demouser2", password: "123456")
+  demo3 = User.create!(name: 'Demo User3', username: "demouser3", password: "123456")
+  demo4 = User.create!(name: 'Demo User4', username: "demouser4", password: "123456")
+  demo5 = User.create!(name: 'Demo User5', username: "demouser5", password: "123456")
+  demo6 = User.create!(name: 'Demo User6', username: "demouser6", password: "123456")
+  demo7 = User.create!(name: 'Demo User7', username: "demouser7", password: "123456")
+  demo8 = User.create!(name: 'Demo User8', username: "demouser8", password: "123456")
+  demo9 = User.create!(name: 'Demo User9', username: "demouser9", password: "123456")
+  demo10 = User.create!(name: 'Demo User10', username: "demouser10", password: "123456")
 
 end
