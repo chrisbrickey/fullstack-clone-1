1337b7551dbfbd304ea839fc3766f5fc956e1e17
chrisbrickey <cbrickey1@gmail.com>
2017-07-22 18:47:28 -0700

    add sample seed photos for testing during development

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,25 @@ ActiveRecord::Base.transaction do
   demo9 = User.create!(name: 'Demo User9', username: "demouser9", password: "123456")
   demo10 = User.create!(name: 'Demo User10', username: "demouser10", password: "123456")
 
+  photo1 = Photo.create!(
+    photo_url: 'https://res.cloudinary.com/dckkkjkuz/image/upload/v1500771271/fierce_xyjq3a.jpg',
+    caption: "ballerina en pointe",
+    location: "earth",
+    author_id: 1
+    )
+
+  photo2 = Photo.create!(
+    photo_url: 'https://res.cloudinary.com/dckkkjkuz/image/upload/v1500771420/infinity_neg30m.jpg',
+    caption: "infinitely beautiful",
+    location: "earth",
+    author_id: 1
+    )
+
+  photo3 = Photo.create!(
+    photo_url: 'https://res.cloudinary.com/dckkkjkuz/image/upload/v1500771107/mural_c8cg36.jpg',
+    caption: "urban inspiration",
+    location: "earth",
+    author_id: 1
+    )
+
 end
