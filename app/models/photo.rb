cf14dc64ca0222a4bdd63764e799f9b8f9206e03
chrisbrickey <cbrickey1@gmail.com>
2017-07-22 21:00:39 -0700

    add detail to author assocation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,14 @@
 class Photo < ApplicationRecord
   validates :photo_url, :author_id, presence: true
 
-  belongs_to :author
+  belongs_to :author,
+    primary_key: :id,
+    foreign_key: :author_id, 
+    class_name: "User"
+
+
   has_many :likes, as: :likable
   has_many :comments
 
-  
+
 end
