ff19d689f4a1afc1b0a5b18dfa4a2bfa12907d74
chrisbrickey <cbrickey1@gmail.com>
2017-07-23 13:00:04 -0700

    make html easier to read by incrementing with two tabs instead of one

--- frontend/components/Signup/signup.jsx
+++ frontend/components/Signup/signup.jsx
@@ -111,158 +111,158 @@ class Signup extends React.Component {
 
       <div className="auth-container">
 
-        <div className="auth-outer">
-          <div className="auth-inner">
+          <div className="auth-outer">
+              <div className="auth-inner">
 
 
-            <div className="left">
+                  <div className="left">
+                      <div className="hero">
 
-              <div className="hero">
-                <img
-                  src={'https://res.cloudinary.com/dckkkjkuz/image/upload/c_scale,w_250/v1500759487/succulent_hero_pixlr_bestcrop_qiirow.png'}
-                  alt="iPhone App"/>
+                          <img
+                              src={'https://res.cloudinary.com/dckkkjkuz/image/upload/c_scale,w_250/v1500759487/succulent_hero_pixlr_bestcrop_qiirow.png'}
+                              alt="iPhone App"/>
 
-                <div className="heroTransition">
-                    <figure></figure>
-                    <figure></figure>
-                    <figure></figure>
-                    <figure></figure>
-                </div>
-
-              </div>
-
-            </div>
-
-
-            <div className="right">
-              <div className="subright">
-
-                  <div className="auth-form-container">
-                    <section className="auth-form-contents">
-
-                      <img
-                        src={'https://res.cloudinary.com/dckkkjkuz/image/upload/v1500659742/InstagramLogo_kgliwu.png'}
-                        alt="Instagram Logo"
-                        className="logo"/>
-
-
-                        {this.props.formType === ('signup') ? (
-                          <div className="copy-top" id="bold">
-                            <p>Sign up to see photos </p>
-                            <p>from your friends.</p>
+                          <div className="heroTransition">
+                              <figure></figure>
+                              <figure></figure>
+                              <figure></figure>
+                              <figure></figure>
                           </div>
-                        ) : (
-                          <div></div>
-                        )}
 
-
-                      <input type="submit" value="Demo login" className="auth-button" onClick={this.demoLogin}/>
-                      <br/>
-
-                      <div className="copy-mid">
-                        <div className="line"/>
-                          <div className="empty-box"/>
-                           <p className="OR">{" "}OR{" "}</p>
-                          <div className="empty-box"/>
-                        <div className="line"/>
                       </div>
-
-                      <form onSubmit={this.handleSubmit} id="auth-button">
-
-                        {this.props.formType === ('signup') ? (
-                          <label>
-                              <input
-                                type="text"
-                                placeholder="Full Name"
-                                name="name"
-                                value={this.state.user.name}
-                                onChange={this.updateUser}
-                                className="login-input"
-                              />
-                          </label>
-                        ) : (
-                          <div id="conditional-input"></div>
-                        )}
-
-
-                          <br/>
-
-                          <label>
-                            <input
-                              type="text"
-                              placeholder="Username"
-                              name="username"
-                              value={this.state.user.username}
-                              onChange={this.updateUser}
-                              className="login-input"
-                            />
-                          </label>
-
-                          <br/>
-
-                          <label>
-                            <input
-                              type="password"
-                              placeholder="Password"
-                              name="password"
-                              value={this.state.user.password}
-                              onChange={this.updateUser}
-                              className="login-input"
-                            />
-                          </label>
-
-                          <br/>
-                          <input
-                            type="submit"
-                            value={buttonText}
-                            className="auth-button"
-                            id="tied-to-form"/>
-                        </form>
-
-
-                          {this.renderErrors()}
-
-
-                        <div className="copy-bottom">
-                          <p>By signing up, you agree to our</p>
-                          <p id="bold" >Terms & Privacy Policy.</p>
-                        </div>
-
-                    </section>
-
                   </div>
 
-                  <section className="toggle-container">
-                    <nav className="toggle-contents">
-                      <p>{`${prefixText}`}
-                        <Link className="auth-link-toggle" to={`${linkPath}`}>{' '}{linkText}</Link>
-                      </p>
-                    </nav>
-                  </section>
 
-                </div>
+                  <div className="right">
+                    <div className="subright">
+
+                        <div className="auth-form-container">
+                            <section className="auth-form-contents">
+
+                                <img
+                                  src={'https://res.cloudinary.com/dckkkjkuz/image/upload/v1500659742/InstagramLogo_kgliwu.png'}
+                                  alt="Instagram Logo"
+                                  className="logo"/>
+
+                                {this.props.formType === ('signup') ? (
+                                  <div className="copy-top" id="bold">
+                                    <p>Sign up to see photos </p>
+                                    <p>from your friends.</p>
+                                  </div>
+                                ) : (
+                                  <div></div>
+                                )}
+
+                                <input type="submit" value="Demo login" className="auth-button" onClick={this.demoLogin}/>
+                                <br/>
+
+                                <div className="copy-mid">
+                                  <div className="line"/>
+                                    <div className="empty-box"/>
+                                     <p className="OR">{" "}OR{" "}</p>
+                                    <div className="empty-box"/>
+                                  <div className="line"/>
+                                </div>
+
+
+                                <form onSubmit={this.handleSubmit} id="auth-button">
+
+                                    {this.props.formType === ('signup') ? (
+                                      <label>
+                                          <input
+                                            type="text"
+                                            placeholder="Full Name"
+                                            name="name"
+                                            value={this.state.user.name}
+                                            onChange={this.updateUser}
+                                            className="login-input"
+                                          />
+                                      </label>
+                                      ) : (
+                                          <div id="conditional-input"></div>
+                                      )}
+
+
+                                      <br/>
+
+                                      <label>
+                                          <input
+                                              type="text"
+                                              placeholder="Username"
+                                              name="username"
+                                              value={this.state.user.username}
+                                              onChange={this.updateUser}
+                                              className="login-input"
+                                          />
+                                      </label>
+
+                                      <br/>
+
+                                      <label>
+                                          <input
+                                              type="password"
+                                              placeholder="Password"
+                                              name="password"
+                                              value={this.state.user.password}
+                                              onChange={this.updateUser}
+                                              className="login-input"
+                                          />
+                                      </label>
+
+                                      <br/>
+                                      <input
+                                          type="submit"
+                                          value={buttonText}
+                                          className="auth-button"
+                                          id="tied-to-form"
+                                      />
+
+                                  </form>
+
+
+                                  {this.renderErrors()}
+
+
+                                <div className="copy-bottom">
+                                    <p>By signing up, you agree to our</p>
+                                    <p id="bold" >Terms & Privacy Policy.</p>
+                                </div>
+
+                            </section>
+
+                        </div>
+
+                        <section className="toggle-container">
+                            <nav className="toggle-contents">
+                                <p>{`${prefixText}`}
+                                    <Link className="auth-link-toggle" to={`${linkPath}`}>{' '}{linkText}</Link>
+                                </p>
+                            </nav>
+                        </section>
+
+                      </div>
+                  </div>
 
               </div>
           </div>
-        </div>
 
 
-        <footer>
-          <ul className="navLinks">
-            <li>ABOUT US</li>
-            <li>SUPPORT</li>
-            <li>BLOG</li>
-            <li>PRESS</li>
-            <li>API</li>
-            <li>JOBS</li>
-            <li>PRIVACY</li>
-            <li>TERMS</li>
-            <li>DIRECTORY</li>
-            <li>LANGUAGE</li>
-            <li>{"      "}</li>
-            <li id="copyright"> &copy; 2017 INSTAGRAM</li>
-          </ul>
-        </footer>
+          <footer>
+              <ul className="navLinks">
+                  <li>ABOUT US</li>
+                  <li>SUPPORT</li>
+                  <li>BLOG</li>
+                  <li>PRESS</li>
+                  <li>API</li>
+                  <li>JOBS</li>
+                  <li>PRIVACY</li>
+                  <li>TERMS</li>
+                  <li>DIRECTORY</li>
+                  <li>LANGUAGE</li>
+                  <li>{"      "}</li>
+                  <li id="copyright"> &copy; 2017 INSTAGRAM</li>
+              </ul>
+          </footer>
 
 
       </div>
