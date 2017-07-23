faead138e25e51197f28cfde5f3ce119da136921
chrisbrickey <cbrickey1@gmail.com>
2017-07-23 12:51:25 -0700

    refactor creation of array of photos for display

--- frontend/components/Home/home.jsx
+++ frontend/components/Home/home.jsx
@@ -5,8 +5,6 @@ import merge from 'lodash/merge';
 
 console.log("on the home.jsx");
 
-
-//if having trouble, remove errors from local state
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -33,71 +31,59 @@ class Home extends React.Component {
 
     // console.log(this.props.loggedIn);
     // console.log(this.props.photos);
-
-    const makeArray = (someObject) => {
-      return Object.keys(someObject).map( (id) => {
-        return someObject[id];
-      });
-    };
-
     const photoFeedObject = this.props.photos.byId;
-    let photoFeedArray = [];
     let photoFeedList = null;
 
     if (photoFeedObject) {
-      photoFeedArray = makeArray(photoFeedObject);
-      console.log(photoFeedArray);
-
-      photoFeedList = photoFeedArray.map( (photoObject) => {
-        return (
-          <li key={photoObject.id} className="feedItem-container">
-            <img src={photoObject.photoUrl} alt="photo"/>
-          </li>
-        );
-      });
+        photoFeedList = Object.keys(photoFeedObject).map( (id) => {
+            return (
+                <li key={photoFeedObject[id].id} className="feedItem-container">
+                    <img src={photoFeedObject[id].photoUrl} alt="photo"/>
+                </li>
+            );
+        });
     }
 
 
     return (
       <div>rendering from home.jsx
 
-        <header>
-          <p>header will go here</p>
-        </header>
+          <header>
+              <p>header will go here</p>
+          </header>
 
-        <main className="photoFeed">
-          
-          <p>picture feed will go here</p>
-          <ul>
-            {photoFeedList}
-          </ul>
+          <main className="photoFeed">
 
-        </main>
+              <p>picture feed will go here</p>
+              <ul>
+                  {photoFeedList}
+              </ul>
+
+          </main>
 
 
+          <footer>
 
-        <footer>
+              <ul className="navLinks">
+                  <li>ABOUT US</li>
+                  <li>SUPPORT</li>
+                  <li>BLOG</li>
+                  <li>PRESS</li>
+                  <li>API</li>
+                  <li>JOBS</li>
+                  <li>PRIVACY</li>
+                  <li>TERMS</li>
+                  <li>DIRECTORY</li>
+                  <li>LANGUAGE</li>
+                  <li>{"      "}</li>
+                  <li id="copyright"> &copy; 2017 INSTAGRAM</li>
+              </ul>
 
-          <ul className="navLinks">
-            <li>ABOUT US</li>
-            <li>SUPPORT</li>
-            <li>BLOG</li>
-            <li>PRESS</li>
-            <li>API</li>
-            <li>JOBS</li>
-            <li>PRIVACY</li>
-            <li>TERMS</li>
-            <li>DIRECTORY</li>
-            <li>LANGUAGE</li>
-            <li>{"      "}</li>
-            <li id="copyright"> &copy; 2017 INSTAGRAM</li>
-          </ul>
+              <section>
+                  <button onClick={this.props.logoutDestroySession} className="logout-button">Logout</button>
+              </section>
 
-          <section>
-            <button onClick={this.props.logoutDestroySession} className="logout-button">Logout</button>
-          </section>
-
-        </footer>
+          </footer>
 
       </div>
     );
