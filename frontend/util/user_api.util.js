84cb5abe3b127da9b594f4fc6b4a5757704f1835
chrisbrickey <cbrickey1@gmail.com>
2017-07-24 00:37:47 -0700

    reverse order of functions to align with other util files

--- frontend/util/user_api.util.js
+++ frontend/util/user_api.util.js
@@ -1,20 +1,19 @@
-export const ajaxFetchSingleUser = (userId) => {
-  console.log("in ajaxFetchSingleUser");
-  return(
-    $.ajax({
-      method: 'GET',
-      url: `api/users/${userId}`
-    })
-  );
-};
-
-
 export const ajaxFetchAllUsers = () => {
   console.log("in ajaxFetchAllUsers");
   return(
     $.ajax({
       method: 'GET',
       url: 'api/users'
+    })
+  );
+};
+
+export const ajaxFetchSingleUser = (userId) => {
+  console.log("in ajaxFetchSingleUser");
+  return(
+    $.ajax({
+      method: 'GET',
+      url: `api/users/${userId}`
     })
   );
 };
