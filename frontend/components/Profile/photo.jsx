800cbf35150a06531df186c3c9e1ad3d23422d5d
chrisbrickey <cbrickey1@gmail.com>
2017-07-25 01:57:13 -0700

    start building out photo detail page

--- frontend/components/Profile/photo.jsx
+++ frontend/components/Profile/photo.jsx
@@ -11,20 +11,55 @@ console.log("on the photo.jsx");
 class Photo extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   photo: {
-    //     caption: "",
-    //     location: "",
-    //     photo_url: ""
-    //   },
-    // };
+    this.state = {
+      photo: {
+        caption: "",
+        location: "",
+        photo_url: ""
+      },
+    };
 
     //this.anyMethod = this.anyMethod.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updatePhoto = this.editPhoto.bind(this);
   }
 
 
   componentDidMount() {
       this.props.fetchAllPhotos();
+      this.props.fetchSinglePhoto(this.props.match.params.photoId);
+    }
+
+    componentWillReceiveProps(nextProps) {
+    if (this.props.match.params.photoId !== nextProps.match.params.photoId) {
+      this.props.fetchSinglePhoto(nextProps.match.params.photoId);
+    }
+  }
+
+    editPhoto(event) {
+      console.log("inside updatePhoto on upload page");
+      const newPhoto = merge({}, this.state);
+      newPhoto.photo[event.target.name] = event.target.value;
+      this.setState(newPhoto);
+    }
+
+    handleSubmit(event) {
+      console.log("inside handleSubmit on photo page");
+
+      let testPhoto = this.state.photo;
+      console.log(testPhoto);
+      console.log({ photo: testPhoto });
+
+      event.preventDefault();
+
+      let newPhoto = this.state.photo;
+      this.props.updatePhoto({ photo: testPhoto })
+        .then(
+          (()=> {
+            console.log("inside handleSubmit callback on photo page upon success");
+            this.setState({ photo: { caption: "", location: "", photo_url: ""} });
+          })
+        );
     }
 
 
@@ -33,6 +68,9 @@ class Photo extends React.Component {
 
     console.log("rendering on photo.jsx");
     console.log(this.props);
+
+    const paramId = this.props.match.params.photoId;
+    const pulledPhoto = this.props.photos[paramId];
 
     return (
       <div className="photo-page-container">
@@ -53,7 +91,80 @@ class Photo extends React.Component {
                   <div className="detail-container">
                     <div className="box1">
                         <div className="optionalText">placeholder text</div>
-                        <div className="another-container"/>
+                        <div className="another-container">
+
+                          <form onSubmit={this.handleSubmit} id="upload-form">
+
+                              <label>
+                                  <input
+                                    type="text"
+                                    placeholder="caption (optional)"
+                                    name="caption"
+                                    value={this.state.photo.caption}
+                                    onChange={this.editPhoto}
+                                    className="upload-input"
+                                  />
+                              </label>
+                              <br/>
+
+                              <label>
+                                  <input
+                                      type="text"
+                                      placeholder="location (optional)"
+                                      name="location"
+                                      value={this.state.photo.location}
+                                      onChange={this.editPhoto}
+                                      className="upload-input"
+                                  />
+                              </label>
+                              <br/>
+
+                                <label>
+                                    <input
+                                        type="text"
+                                        placeholder="photo's url (required)"
+                                        name="photo_url"
+                                        value={this.state.photo.photo_url}
+                                        onChange={this.editPhoto}
+                                        className="upload-input"
+                                    />
+                                </label>
+                              <br/>
+                              <br/>
+
+
+                              <button
+                                onClick={this.handleSubmit}
+                                type="submit"
+                                value="Submit"
+                                className="manual-submit">
+                                Submit Changes
+                              </button>
+
+                          </form>
+
+
+
+
+                          <button
+                            onClick={this.props.destroyPhoto}
+                            type="submit"
+                            value="Submit"
+                            className="manual-submit">
+                            Delete Post
+                          </button>
+
+
+
+
+
+
+
+
+
+
+                        </div>
+
                     </div>
 
 
@@ -61,6 +172,10 @@ class Photo extends React.Component {
                         <div className="optionalText">placeholder text</div>
 
                         <div className="yet-another-container">
+
+
+
+
                         </div>
                     </div>
                   </div>
