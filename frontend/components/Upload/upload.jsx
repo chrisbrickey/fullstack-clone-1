f5b723c7f0de08f6acb4f76876629f2dfb58f2bb
chrisbrickey <cbrickey1@gmail.com>
2017-07-26 03:15:07 -0700

    clean up comments

--- frontend/components/Upload/upload.jsx
+++ frontend/components/Upload/upload.jsx
@@ -23,35 +23,21 @@ class Upload extends React.Component {
         location: "",
         photo_url: "placeholder"
       },
-      // uploadedFileCloudinaryUrl: "filler"
     };
 
-    //this.anyMethod = this.anyMethod.bind(this);
-    // this.cropPhoto = this.cropPhoto.bind(this);
-    this.postPhoto = this.postPhoto.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updatePhoto = this.updatePhoto.bind(this);
-    // this.onImageDrop = this.onImageDrop.bind(this);
-    // this.handleImageUpload = this.handleImageUpload.bind(this);
   }
-
 
   componentDidMount() {
       this.props.fetchAllPhotos();
-    }
-
-  onImageDrop(files) {
-    console.log("inside ImageDrop");
-    console.log(files);
-    this.handleImageUpload(files[0]);
-
   }
 
+  onImageDrop(files) {
+    this.handleImageUpload(files[0]);
+  }
 
   handleImageUpload(file) {
-    console.log("inside handleImageUpload");
-    console.log(file);
-
     let upload = request.post(CLOUDINARY_UPLOAD_URL)
                           .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
                           .field('file', file);
@@ -61,8 +47,6 @@ class Upload extends React.Component {
           console.log(`error resulting from cloudinary upload: ${err}`);
         }
 
-        console.log("not throwing errors");
-
         if (response.body.secure_url !== 'placeholder') {
           this.setState({ photo: { photo_url: response.body.secure_url}});
           console.log(this.state);
@@ -71,26 +55,9 @@ class Upload extends React.Component {
       });
   }
 
-  // cropPhoto(photoUrl) {
-  //   const cropText = "/upload/c_thumb,w_600/";
-  //   // const cropText = "/upload/c_thumb,h_500,w_500/";
-  //   const photoUrlArray = photoUrl.split("/upload/");
-  //   const croppedUrl = photoUrlArray[0] + cropText + photoUrlArray[1];
-  //
-  //   return croppedUrl;
-  // }
-
-  //for upload via cloudinary
-  postPhoto(photoUrl) {
-    console.log("inside postPhoto on upload.jsx");
-    let newPhoto = {photo_url: photoUrl};
-    console.log(newPhoto);
-    this.props.createPhoto({photo: newPhoto});
-  }
-
-
   updatePhoto(event) {
     console.log("inside updatePhoto on upload page");
+
     const newPhoto = merge({}, this.state);
     newPhoto.photo[event.target.name] = event.target.value;
     this.setState(newPhoto);
@@ -100,8 +67,6 @@ class Upload extends React.Component {
     console.log("inside handleSubmit on upload page");
 
     let testPhoto = this.state.photo;
-    console.log(testPhoto);
-    console.log({ photo: testPhoto });
 
     event.preventDefault();
 
@@ -115,11 +80,7 @@ class Upload extends React.Component {
       );
   }
 
-  //remember that errors and other objects might be null so render conditionally
   render() {
-
-    console.log("rendering on upload.jsx");
-    console.log(this.props);
 
     return (
       <div className="upload-page-container">
@@ -130,113 +91,6 @@ class Upload extends React.Component {
           <section className="upload-sub-container">
               <div className="sub-sub-container">
                   <div className="upload-form-container">
-
-
-
-                    <section className="option1-box">
-
-                        <div className="optionText">Option Dropzone: working on this</div>
-
-
-                        <div className="cloudinary-container">
-
-                            <Dropzone
-                                multiple={false}
-                                accept="image/*"
-                                onDrop={this.onImageDrop.bind(this)}>
-                                <p>Drop an image or click to select a file to upload.</p>
-                            </Dropzone>
-
-                            <div>
-                                {this.state.photo.photo_url === 'placeholder' ? null :
-                                    (<div>
-                                        <img src={this.state.photo.photo_url} />
-                                     </div>)}
-                            </div>
-
-                        </div>
-
-
-
-                    </section>
-
-
-
-
-
-                      <section className="option1-box">
-
-                          <div className="optionText">Option 1: select file here</div>
-
-                          <div className="cloudinary-container">
-                            <CloudWidget postPhoto={this.postPhoto}/>
-                          </div>
-
-                      </section>
-
-
-
-                      <section className="option2-box">
-
-                          <div className="optionText">Option 2: enter information manually</div>
-
-                          <div className="options-form-container">
-                              <form onSubmit={this.handleSubmit} id="upload-form">
-
-                                  <label>
-                                      <input
-                                        type="text"
-                                        placeholder="caption (optional)"
-                                        name="caption"
-                                        value={this.state.photo.caption}
-                                        onChange={this.updatePhoto}
-                                        className="upload-input"
-                                      />
-                                  </label>
-                                  <br/>
-
-                                  <label>
-                                      <input
-                                          type="text"
-                                          placeholder="location (optional)"
-                                          name="location"
-                                          value={this.state.photo.location}
-                                          onChange={this.updatePhoto}
-                                          className="upload-input"
-                                      />
-                                  </label>
-                                  <br/>
-
-                                    <label>
-                                        <input
-                                            type="text"
-                                            placeholder="photo's url (required)"
-                                            name="photo_url"
-                                            value={this.state.photo.photo_url}
-                                            onChange={this.updatePhoto}
-                                            className="upload-input"
-                                        />
-                                    </label>
-                                  <br/>
-                                  <br/>
-
-
-                                  <button
-                                    onClick={this.handleSubmit}
-                                    type="submit"
-                                    value="Submit"
-                                    className="manual-submit">
-                                    Submit
-                                  </button>
-
-                              </form>
-
-                          </div>
-
-
-                      </section>
-
-
 
                       <section className="option1-box">
 
@@ -295,7 +149,7 @@ class Upload extends React.Component {
                                     type="submit"
                                     value="Submit"
                                     className="manual-submit">
-                                    Submit
+                                    Post it
                                   </button>
                               </form>
 
