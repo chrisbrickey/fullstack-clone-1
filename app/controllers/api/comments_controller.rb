2368314805655e27f521df28d98b559057a58189
chrisbrickey <cbrickey1@gmail.com>
2017-07-27 22:41:57 -0700

    draft comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,44 @@
 class Api::CommentsController < ApplicationController
+
+def create
+  @comment = Comment.new(comment_params)
+  @comment.user_id = current_user.id
+
+  if @comment.save
+    render "api/comments/show"
+  else
+    render(
+      json: @comment.errors.full_messages,
+      status: 422
+    )
+  end
+
+end
+
+
+#should I be destroying by params[:id] instead?
+def destroy
+  @comment = Comment.find_by(comment_params)
+
+  if @comment.destroy
+    render "api/comments/show"
+  else
+    render(
+      json: @comment.errors.full_messages,
+      status: 422
+    )
+  end
+
+end
+
+
+
+
+private
+
+#may need to change spelling of photo_id
+def comment_params
+  params.require(:comment).permit(:body, :photo_id)
+end
+
 end
