d3cdf41b82c3764435a2c6bd9a025189a7f72685
chrisbrickey <cbrickey1@gmail.com>
2017-07-28 00:17:30 -0700

    draft comment action creators

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,51 @@
+import { ajaxCreateComment,
+         ajaxDestroyComment } from "../util/comment_api_util";
+
+
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+
+//synchronous action creators
+
+export const receiveComment = comment => {
+  console.log("inside sync receiveComment");
+  console.log(comment);
+
+  return({
+    type: RECEIVE_COMMENT,
+    comment
+  });
+};
+
+
+export const removeComment = comment => {
+  console.log("inside sync removeComment");
+  console.log(comment);
+
+  return({
+    type: REMOVE_COMMENT,
+    comment
+  });
+};
+
+
+
+//asynchronous action creators
+
+export const createComment = (comment) => dispatch => {
+  console.log("in async createComment");
+
+  return ajaxCreateComment(comment)
+    .then( returnedComment => (dispatch(receiveComment(returnedComment)))
+  );
+};
+
+
+export const destroyComment = (comment) => dispatch => {
+  console.log("in async destroyComment");
+
+  return ajaxDestroyComment(comment)
+    .then( commentItDeleted => (dispatch(removeComment(commentItDeleted)))
+  );
+};
