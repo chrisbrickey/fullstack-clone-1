56f50a3f3b8278bdfc5dd8a3d59015c96f8b1635
chrisbrickey <cbrickey1@gmail.com>
2017-07-28 15:36:12 -0700

    exclude redux-logger from production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,6 +5,14 @@ import logger from 'redux-logger';
 
 import { rootReducer } from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
 export const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
