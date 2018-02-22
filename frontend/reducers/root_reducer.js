aeec67a07b787807410cdf19d1074a46e99bbc36
chrisbrickey <cbrickey1@gmail.com>
2018-02-21 22:11:23 -0800

    add user_reducer file and connect to root_reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { sessionReducer } from './session_reducer';
 import { photoReducer } from './photo_reducer';
+import { userReducer } from './user_reducer';
 
 export const rootReducer = combineReducers ({
   session: sessionReducer,
-  photos: photoReducer
+  photos: photoReducer,
+  users: userReducer
 });
---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,4 @@
+import merge from 'lodash/merge';
+
+import { RECEIVE_ALL_USERS,
+         RECEIVE_SINGLE_USER } from '../actions/user_actions';
